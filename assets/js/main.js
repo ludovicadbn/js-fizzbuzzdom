f7d6183a11a7993c59c4a95b361f169497e15419
dibss <ludovicad01@gmail.com>
2022-03-11 16:09:53 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
-for ( counter=0; counter<=100; counter++ ) {
+/*for ( counter=0; counter<=100; counter++ ) {
   document.getElementById("fbd").innerHTML = `
       <div class="square row jc-center ai-center">
         <div>${counter}</div>
       </div>
     `;
-}
+}*/
 
-function fizzBuzzDom() {
+// ------------------------------------------------------
+
+/*function fizzBuzzDom() {
   let text = "";
   let counter = 1;
 
@@ -29,9 +31,11 @@ function fizzBuzzDom() {
         <div>${text}</div>
       </div>
     `;
-}
+}*/
 
-let counter = 1;
+// --------------------------------------------------------------
+
+/*let counter = 1;
 
 while (counter <= 100){
   document.getElementById("fbd").innerHTML = `
@@ -40,4 +44,31 @@ while (counter <= 100){
     </div>
   `;
   counter += 1;
+}*/
+
+// ---------------------------------------------------------------------
+
+/*let text = "";
+
+for ( counter=0; counter<=100; counter++ ) {
+  
+  if (counter % 15 == 0) {
+    text += "FizzBuzz";
+  } else if (counter % 3 == 0) {
+    text += "Fizz";
+  } else if (counter % 5 == 0) {
+    text += "Buzz";
+  } else {
+    text += counter;
+  }
+
+  document.getElementById("fbd").innerHTML = `
+    <div class="row jc-center ai-center">
+      <div id="text" class="p-3">${text}</div>
+    </div>
+  `;
 }
+
+if (text == "Buzz"){
+  document.getElementById("text").innerHTML = `style="background-color: yellow;"`
+}*/
