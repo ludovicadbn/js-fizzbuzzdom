7bbb1fc7c0a5fdadf9c5e8783d9c8a1e11428aa5
dibss <ludovicad01@gmail.com>
2022-03-11 15:51:46 +0100

    js

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+for ( counter=0; counter<=100; counter++ ) {
+  document.getElementById("fbd").innerHTML = `
+      <div class="square row jc-center ai-center">
+        <div>${counter}</div>
+      </div>
+    `;
+}
+
+function fizzBuzzDom() {
+  let text = "";
+  let counter = 1;
+
+  while (counter <= 100){
+    counter += 1;
+  }
+
+  if (counter % 15 == 0) {
+    text += "FizzBuzz";
+  } else if (counter % 3 == 0) {
+    text += "Fizz";
+  } else if (counter % 5 == 0) {
+    text += "Buzz";
+  } else {
+    text += counter;
+  }
+
+  document.getElementById("fbd").innerHTML = `
+      <div class="square row jc-center ai-center">
+        <div>${text}</div>
+      </div>
+    `;
+}
+
+let counter = 1;
+
+while (counter <= 100){
+  document.getElementById("fbd").innerHTML = `
+    <div class="square row jc-center ai-center">
+      <div>${counter}</div>
+    </div>
+  `;
+  counter += 1;
+}
